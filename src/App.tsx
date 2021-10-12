81c1dcd0148dc823a1a5a783e512732c44147963
François Georgy <francois.georgy@gmail.com>
2021-10-12 16:56:08 +0200

    add basic stores and MIDI support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import {MidiPortsSelect} from "./components/midi/MidiPortsSelect";
+import {ExampleMIDIDevices} from "./components/ExampleMIDIDevices";
+import {ExampleMIDIMessages} from "./components/ExampleMIDIMessages";
 
 function App() {
     return (
         <div className="App">
             <header>
-                WebMIDI Application
+                <h1>WebMIDI Application</h1>
             </header>
+            <div>
+                <MidiPortsSelect />
+                <ExampleMIDIDevices />
+                <ExampleMIDIMessages />
+            </div>
         </div>
     )
 }
